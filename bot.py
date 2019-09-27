1fb1084a9dd717b09de3dc2afe153392a166b81d
Marcos Calvo García <53514344+mcalvog@users.noreply.github.com>
2019-09-27 10:13:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ def send_welcome(message):
     bot.reply_to(message, "Welcome ' + message.from_user.first_name + '! Start by sending me pictures of your food.")
 
 @bot.message_handler(commands=['help'])
-def send_welcome(message):
+def send_help(message):
     bot.reply_to(message, "Simply send me a picture of your foods. Then use /weekly or /daily commands to view the kcal report.")
 
 bot.polling()
