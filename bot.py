23107dfd06ba172aa47edd47aede4812d5bbda6f
Marcos Calvo García <53514344+mcalvog@users.noreply.github.com>
2019-09-27 09:48:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
+import os
 import telebot
+from flask import Flask, request
 
 bot = telebot.TeleBot(os.environ["BOT_KEY"])
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
