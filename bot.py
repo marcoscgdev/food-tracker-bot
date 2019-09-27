f6e972f8fea9986156c949c15eb208ae116f9a4e
Marcos Calvo García <53514344+mcalvog@users.noreply.github.com>
2019-09-27 10:12:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(os.environ["BOT_KEY"])
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Welcome ' + message.entities[0].user.first_name + '! Start by sending me pictures of your food.")
+    bot.reply_to(message, "Welcome ' + message.from_user.first_name + '! Start by sending me pictures of your food.")
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
