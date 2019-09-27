492fa852d00cbbfb5d723cf4e10e74390276cc79
Marcos Calvo García <53514344+mcalvog@users.noreply.github.com>
2019-09-27 10:04:47 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,12 +3,13 @@ import telebot
 from flask import Flask, request
 
 bot = telebot.TeleBot(os.environ["BOT_KEY"])
-app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Welcome ' + message.entities[0].user.first_name + '! Start by sending me pictures of your food.")
 
-if __name__ == '__main__':  
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, "Simply send me a picture of your foods. Then use /weekly or /daily commands to view the kcal report.")
+
+bot.polling()
