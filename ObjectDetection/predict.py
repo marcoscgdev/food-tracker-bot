546a9f24d274dd51f4925e7a6fe15a26042188f4
Marcos Calvo García <marcoscgdev@gmail.com>
2019-12-10 18:20:23 +0100

    Update predict.py

--- ObjectDetection/predict.py
+++ ObjectDetection/predict.py
@@ -19,29 +19,21 @@ from keras_model import build_model
 
 if __name__ == '__main__':
 
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    MEAN = np.array([51.072815, 51.072815, 51.072815])
-    STD = np.array([108.75629,  92.98068,  85.61884])
+	def predict(image):
+		os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+		MEAN = np.array([51.072815, 51.072815, 51.072815])
+		STD = np.array([108.75629,  92.98068,  85.61884])
+		categories = [
+		'healthy', 'junk', 'dessert', 'appetizer', 'mains', 'soups', 'carbs', 'protein', 'fats', 'meat'
+		]
 
-    categories = [
-    'healthy', 'junk', 'dessert', 'appetizer', 'mains', 'soups', 'carbs', 'protein', 'fats', 'meat'
-    ]
+		model = build_model('inference', model_path = 'http://insertbrain.universidadsanjorge.net/uploads/model.h5')
+		img = np.expand_dims(cv2.imread(image, 1), axis = 0)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--image', help = 'Path to the image to be predicted', required = True)
-    parser.add_argument('--saved_model', help = 'Path of the saved model', required = True)
+		for i in range(3):
+			img[:, :, :, i] = (img[:, :, :, i] - MEAN[i]) / STD[i]
 
-    args = parser.parse_args()
+		prediction = np.round(model.predict(img)[0])
+		labels = [categories[idx] for idx, current_prediction in enumerate(prediction) if current_prediction == 1]
 
-    model = build_model('inference', model_path = args.saved_model)
-
-    img = np.expand_dims(cv2.imread(args.image, 1), axis = 0)
-
-    for i in range(3):
-        img[:, :, :, i] = (img[:, :, :, i] - MEAN[i]) / STD[i]
-
-    prediction = np.round(model.predict(img)[0])
-    labels = [categories[idx] for idx, current_prediction in enumerate(prediction) if current_prediction == 1]
-
-    print('Prediction:', labels)
-
+		print('Prediction:', labels)
