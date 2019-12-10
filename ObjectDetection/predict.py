8503b0c5b5263c9ea1e6383d6855cfc4f725b857
Marcos Calvo García <marcoscgdev@gmail.com>
2019-12-10 18:42:14 +0100

    Import keras libs

--- ObjectDetection/predict.py
+++ ObjectDetection/predict.py
@@ -17,23 +17,21 @@ from sklearn.model_selection import train_test_split
 
 from keras_model import build_model
 
-if __name__ == '__main__':
+def predict(image):
+	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+	MEAN = np.array([51.072815, 51.072815, 51.072815])
+	STD = np.array([108.75629,  92.98068,  85.61884])
+	categories = [
+	'healthy', 'junk', 'dessert', 'appetizer', 'mains', 'soups', 'carbs', 'protein', 'fats', 'meat'
+	]
 
-	def predict(image):
-		os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-		MEAN = np.array([51.072815, 51.072815, 51.072815])
-		STD = np.array([108.75629,  92.98068,  85.61884])
-		categories = [
-		'healthy', 'junk', 'dessert', 'appetizer', 'mains', 'soups', 'carbs', 'protein', 'fats', 'meat'
-		]
+	model = build_model('inference', model_path = 'http://insertbrain.universidadsanjorge.net/uploads/model.h5')
+	img = np.expand_dims(cv2.imread(image, 1), axis = 0)
 
-		model = build_model('inference', model_path = 'http://insertbrain.universidadsanjorge.net/uploads/model.h5')
-		img = np.expand_dims(cv2.imread(image, 1), axis = 0)
+	for i in range(3):
+		img[:, :, :, i] = (img[:, :, :, i] - MEAN[i]) / STD[i]
 
-		for i in range(3):
-			img[:, :, :, i] = (img[:, :, :, i] - MEAN[i]) / STD[i]
+	prediction = np.round(model.predict(img)[0])
+	labels = [categories[idx] for idx, current_prediction in enumerate(prediction) if current_prediction == 1]
 
-		prediction = np.round(model.predict(img)[0])
-		labels = [categories[idx] for idx, current_prediction in enumerate(prediction) if current_prediction == 1]
-
-		print('Prediction:', labels)
+	print('Prediction:', labels)
